09a8c97c6d52c66cc34cf922c58f6dad2abdc846
AadilKhan0461 <aadilkhan04610461@gmail.com>
2024-08-06 20:59:18 +0530

    first commit

---
+++ src/features/auth/authService.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const API_URL = "/api/user/";
+
+const register = async(formData) => {
+   const response = await axios.post(API_URL + "register" , formData)
+   localStorage.setItem("user" , JSON.stringify(response.data))
+   return response.data
+}
+
+const login = async(formData) => {
+    const response = await axios.post(API_URL + "login" , formData)
+    localStorage.setItem("user" , JSON.stringify(response.data))
+    return response.data
+ }
+
+ const facebook = async (formData) => {
+   const response = await axios.post(API_URL + "login", formData);
+   localStorage.setItem("user", JSON.stringify(response.data));
+   return response.data;
+ };
+
+ const authService = {
+    register,
+    login,
+    facebook,
+}
+
+export default authService;
